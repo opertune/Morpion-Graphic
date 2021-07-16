ef533c3a27a13a673b1fa607c826df3139d62932
opertune <59347356+opertune@users.noreply.github.com>
2021-07-16 11:07:16 +0200

    Add files via upload

---
+++ Morpionwf/Morpion/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Morpion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnChoix1 = new System.Windows.Forms.Button();
+            this.btnChoix2 = new System.Windows.Forms.Button();
+            this.btnChoix3 = new System.Windows.Forms.Button();
+            this.btnChoix4 = new System.Windows.Forms.Button();
+            this.btnChoix5 = new System.Windows.Forms.Button();
+            this.btnChoix6 = new System.Windows.Forms.Button();
+            this.btnChoix7 = new System.Windows.Forms.Button();
+            this.btnChoix8 = new System.Windows.Forms.Button();
+            this.btnChoix9 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblJoueur = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 299);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnChoix1
+            // 
+            this.btnChoix1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix1.Location = new System.Drawing.Point(12, 37);
+            this.btnChoix1.Name = "btnChoix1";
+            this.btnChoix1.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix1.TabIndex = 1;
+            this.btnChoix1.UseVisualStyleBackColor = true;
+            this.btnChoix1.Click += new System.EventHandler(this.btnChoix1_Click);
+            // 
+            // btnChoix2
+            // 
+            this.btnChoix2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix2.Location = new System.Drawing.Point(112, 37);
+            this.btnChoix2.Name = "btnChoix2";
+            this.btnChoix2.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix2.TabIndex = 2;
+            this.btnChoix2.UseVisualStyleBackColor = true;
+            this.btnChoix2.Click += new System.EventHandler(this.btnChoix2_Click);
+            // 
+            // btnChoix3
+            // 
+            this.btnChoix3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix3.Location = new System.Drawing.Point(212, 37);
+            this.btnChoix3.Name = "btnChoix3";
+            this.btnChoix3.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix3.TabIndex = 3;
+            this.btnChoix3.UseVisualStyleBackColor = true;
+            this.btnChoix3.Click += new System.EventHandler(this.btnChoix3_Click);
+            // 
+            // btnChoix4
+            // 
+            this.btnChoix4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix4.Location = new System.Drawing.Point(12, 137);
+            this.btnChoix4.Name = "btnChoix4";
+            this.btnChoix4.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix4.TabIndex = 4;
+            this.btnChoix4.UseVisualStyleBackColor = true;
+            this.btnChoix4.Click += new System.EventHandler(this.btnChoix4_Click);
+            // 
+            // btnChoix5
+            // 
+            this.btnChoix5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix5.Location = new System.Drawing.Point(112, 137);
+            this.btnChoix5.Name = "btnChoix5";
+            this.btnChoix5.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix5.TabIndex = 5;
+            this.btnChoix5.UseVisualStyleBackColor = true;
+            this.btnChoix5.Click += new System.EventHandler(this.btnChoix5_Click);
+            // 
+            // btnChoix6
+            // 
+            this.btnChoix6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix6.Location = new System.Drawing.Point(212, 137);
+            this.btnChoix6.Name = "btnChoix6";
+            this.btnChoix6.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix6.TabIndex = 6;
+            this.btnChoix6.UseVisualStyleBackColor = true;
+            this.btnChoix6.Click += new System.EventHandler(this.btnChoix6_Click);
+            // 
+            // btnChoix7
+            // 
+            this.btnChoix7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix7.Location = new System.Drawing.Point(12, 237);
+            this.btnChoix7.Name = "btnChoix7";
+            this.btnChoix7.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix7.TabIndex = 7;
+            this.btnChoix7.UseVisualStyleBackColor = true;
+            this.btnChoix7.Click += new System.EventHandler(this.btnChoix7_Click);
+            // 
+            // btnChoix8
+            // 
+            this.btnChoix8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix8.Location = new System.Drawing.Point(112, 237);
+            this.btnChoix8.Name = "btnChoix8";
+            this.btnChoix8.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix8.TabIndex = 8;
+            this.btnChoix8.UseVisualStyleBackColor = true;
+            this.btnChoix8.Click += new System.EventHandler(this.btnChoix8_Click);
+            // 
+            // btnChoix9
+            // 
+            this.btnChoix9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoix9.Location = new System.Drawing.Point(212, 237);
+            this.btnChoix9.Name = "btnChoix9";
+            this.btnChoix9.Size = new System.Drawing.Size(97, 97);
+            this.btnChoix9.TabIndex = 9;
+            this.btnChoix9.UseVisualStyleBackColor = true;
+            this.btnChoix9.Click += new System.EventHandler(this.btnChoix9_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(16, 338);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 31);
+            this.lblResult.TabIndex = 10;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Image = ((System.Drawing.Image)(resources.GetObject("btnRestart.Image")));
+            this.btnRestart.Location = new System.Drawing.Point(22, 380);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(71, 64);
+            this.btnRestart.TabIndex = 11;
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(112, 380);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(71, 64);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblJoueur
+            // 
+            this.lblJoueur.AutoSize = true;
+            this.lblJoueur.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJoueur.Location = new System.Drawing.Point(97, 4);
+            this.lblJoueur.Name = "lblJoueur";
+            this.lblJoueur.Size = new System.Drawing.Size(0, 31);
+            this.lblJoueur.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(323, 456);
+            this.Controls.Add(this.lblJoueur);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnChoix9);
+            this.Controls.Add(this.btnChoix8);
+            this.Controls.Add(this.btnChoix7);
+            this.Controls.Add(this.btnChoix6);
+            this.Controls.Add(this.btnChoix5);
+            this.Controls.Add(this.btnChoix4);
+            this.Controls.Add(this.btnChoix3);
+            this.Controls.Add(this.btnChoix2);
+            this.Controls.Add(this.btnChoix1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnChoix1;
+        private System.Windows.Forms.Button btnChoix2;
+        private System.Windows.Forms.Button btnChoix3;
+        private System.Windows.Forms.Button btnChoix4;
+        private System.Windows.Forms.Button btnChoix5;
+        private System.Windows.Forms.Button btnChoix6;
+        private System.Windows.Forms.Button btnChoix7;
+        private System.Windows.Forms.Button btnChoix8;
+        private System.Windows.Forms.Button btnChoix9;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblJoueur;
+    }
+}
+
---
+++ Morpionwf/Morpion/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Morpion
+{
+    public partial class Form1 : Form
+    {
+        int player = 1, coup = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblJoueur.Text = "Joueur " + player;
+        }
+
+        public bool check()
+        {
+            if (btnChoix1.Text == "X" && btnChoix2.Text == "X" && btnChoix3.Text == "X" ||
+                btnChoix4.Text == "X" && btnChoix5.Text == "X" && btnChoix6.Text == "X" ||
+                btnChoix7.Text == "X" && btnChoix8.Text == "X" && btnChoix9.Text == "X" ||
+                btnChoix1.Text == "X" && btnChoix5.Text == "X" && btnChoix9.Text == "X" ||
+                btnChoix3.Text == "X" && btnChoix5.Text == "X" && btnChoix7.Text == "X" ||
+                btnChoix1.Text == "X" && btnChoix4.Text == "X" && btnChoix7.Text == "X" ||
+                btnChoix2.Text == "X" && btnChoix5.Text == "X" && btnChoix8.Text == "X" ||
+                btnChoix3.Text == "X" && btnChoix6.Text == "X" && btnChoix9.Text == "X" ||
+                btnChoix1.Text == "O" && btnChoix2.Text == "O" && btnChoix3.Text == "O" ||
+                btnChoix4.Text == "O" && btnChoix5.Text == "O" && btnChoix6.Text == "O" ||
+                btnChoix7.Text == "O" && btnChoix8.Text == "O" && btnChoix9.Text == "O" ||
+                btnChoix1.Text == "O" && btnChoix5.Text == "O" && btnChoix9.Text == "O" ||
+                btnChoix3.Text == "O" && btnChoix5.Text == "O" && btnChoix7.Text == "O" ||
+                btnChoix1.Text == "O" && btnChoix4.Text == "O" && btnChoix7.Text == "O" ||
+                btnChoix2.Text == "O" && btnChoix5.Text == "O" && btnChoix8.Text == "O" ||
+                btnChoix3.Text == "O" && btnChoix6.Text == "O" && btnChoix9.Text == "O")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void vainqueur()
+        {
+            if (check())
+            {
+                if (player == 1)
+                {
+                    player = 2;
+                }
+                else
+                {
+                    player = 1;
+                }
+               lblResult.Text = "Joueur " + player + " gagne !";
+                btnChoix1.Enabled = false;
+                btnChoix2.Enabled = false;
+                btnChoix3.Enabled = false;
+                btnChoix4.Enabled = false;
+                btnChoix5.Enabled = false;
+                btnChoix6.Enabled = false;
+                btnChoix7.Enabled = false;
+                btnChoix8.Enabled = false;
+                btnChoix9.Enabled = false;
+            }
+            if (coup == 9)
+            {
+                lblResult.Text = "Match nul";
+                btnChoix1.Enabled = false;
+                btnChoix2.Enabled = false;
+                btnChoix3.Enabled = false;
+                btnChoix4.Enabled = false;
+                btnChoix5.Enabled = false;
+                btnChoix6.Enabled = false;
+                btnChoix7.Enabled = false;
+                btnChoix8.Enabled = false;
+                btnChoix9.Enabled = false;
+            }
+        }
+        private void btnChoix1_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix1.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix1.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            vainqueur();
+        }
+
+        private void btnChoix2_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix2.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix2.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+
+            vainqueur();
+        }
+
+        private void btnChoix3_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix3.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix3.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            vainqueur();
+        }
+
+        private void btnChoix4_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix4.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix4.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+
+            vainqueur();
+        }
+
+        private void btnChoix5_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix5.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix5.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+
+            vainqueur();
+        }
+
+        private void btnChoix6_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix6.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix6.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+
+            vainqueur();
+        }
+
+        private void btnChoix7_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix7.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix7.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+
+            vainqueur();
+        }
+
+        private void btnChoix8_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix8.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix8.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+
+            vainqueur();
+        }
+
+        private void btnChoix9_Click(object sender, EventArgs e)
+        {
+            if (player == 1)
+            {
+                btnChoix9.Text = "X";
+                player++;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+            else
+            {
+                btnChoix9.Text = "O";
+                player--;
+                coup++;
+                lblJoueur.Text = "Joueur " + player;
+            }
+
+            vainqueur();
+        }
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            btnChoix1.Text = "";
+            btnChoix2.Text = "";
+            btnChoix3.Text = "";
+            btnChoix4.Text = "";
+            btnChoix5.Text = "";
+            btnChoix6.Text = "";
+            btnChoix7.Text = "";
+            btnChoix8.Text = "";
+            btnChoix9.Text = "";
+            lblResult.Text = "";
+            coup = 0;
+            player = 1;
+            lblJoueur.Text = "Joueur " + player;
+
+            btnChoix1.Enabled = true;
+            btnChoix2.Enabled = true;
+            btnChoix3.Enabled = true;
+            btnChoix4.Enabled = true;
+            btnChoix5.Enabled = true;
+            btnChoix6.Enabled = true;
+            btnChoix7.Enabled = true;
+            btnChoix8.Enabled = true;
+            btnChoix9.Enabled = true;
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
